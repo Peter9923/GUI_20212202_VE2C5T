994ac57c8396273355793a7b123dece456e23c1f
PeterSzilagyi <szilagyi.peter98@gmail.com>
2022-04-09 14:53:32 +0200

    Create -> Archer -> Cannon -> Tower classes

---
+++ Knights_And_Fields/Game/Models/Tower.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Game.Models
+{
+    public class Tower : ILivingGameItem
+    {
+        private double centerX;
+        private double centerY;
+
+        private int speedX;
+        private int speedY;
+        private int level;
+
+        public double MaxLife { get; set; }
+        public double ActualLife { get; set; }
+        public int Cost { get { return 100; } }
+        public int Damage { get { return level * 5; } }
+        public int UpgradeCost { get { return level * Cost; } }
+
+        public Tower(double centerX, double centerY)
+        {
+            this.centerX = centerX;
+            this.centerY = centerY;
+            this.speedX = 0; //cant move
+            this.speedY = 0; //cant move
+
+            MaxLife = 10;
+            level = 1;
+        }
+
+        public Geometry Area
+        {
+            get
+            {
+                return new RectangleGeometry(new Rect(centerX, centerY, Config.NegyzetWidth, Config.NegyzetHeight));
+            }
+        }
+
+
+
+        public bool IsCollision(IGameItem other)
+        {
+            return Geometry.Combine(this.Area, other.Area,
+                GeometryCombineMode.Intersect, null).GetArea() > 0;
+        }
+
+        public void SetXY(double x, double y)
+        {
+            this.centerX = x;
+            this.centerY = y;
+        }
+
+        //what happen when collision
+        public void Collision()
+        {
+            ActualLife -= 10;
+        }
+    }
+}
