a4383244bc9595c9ad154bd60b3c4a4c106d5d81
PeterSzilagyi <szilagyi.peter98@gmail.com>
2022-04-22 20:43:35 +0200

    Created Config class in GameModel. Set project reference: GameRenderer --> GameModel

---
+++ Knights_And_Fields/GameModel/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameModel
+{
+    public static class Config
+    {
+        public static double TileSize;
+        public static double GameHeight;
+        public static double GameWidth;
+    }
+}
--- Knights_And_Fields/GameRenderer/Renderer.cs
+++ Knights_And_Fields/GameRenderer/Renderer.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using GameModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace GameRenderer
 {
     public class Renderer
     {
+
     }
 }
