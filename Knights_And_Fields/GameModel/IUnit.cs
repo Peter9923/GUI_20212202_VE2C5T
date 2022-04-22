c44f2b0a93f914e739faf0c5b4e4fca4d56dd916
PeterSzilagyi <szilagyi.peter98@gmail.com>
2022-04-22 20:52:15 +0200

    Creating properties in IUnit interface.

--- Knights_And_Fields/GameModel/IUnit.cs
+++ Knights_And_Fields/GameModel/IUnit.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace GameModel
 {
     public interface IUnit
     {
-
+        double MaxLife { get; set; }
+        double ActualLife { get; set; }
+        int Damage { get; set; }
+        int Speed { get; set; }
+        int Tick { get; set; }
+        Point Position { get; set; }
+        public void Collision();
     }
 }
