b251bc3e77c0033ee28b840fe68f8344fe4676e3
PeterSzilagyi <szilagyi.peter98@gmail.com>
2022-04-22 20:56:09 +0200

    Creating 1 important method and 1 important property in IGameItem. Are --> Get an object area. ISCollision --> If 2 items collision with each other this method return with true value.

--- Knights_And_Fields/GameModel/IGameItem.cs
+++ Knights_And_Fields/GameModel/IGameItem.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace GameModel
 {
     public interface IGameItem
     {
+        Geometry Area { get; }
+        bool IsCollision(IGameItem other);
     }
 }
