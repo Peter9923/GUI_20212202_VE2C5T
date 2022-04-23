8f8d66814ad7b83df3a1c35248f0301a54f66baa
PeterSzilagyi <szilagyi.peter98@gmail.com>
2022-04-23 18:52:52 +0200

    Creating EnemyKnight class.

---
+++ Knights_And_Fields/GameModel/EnemyKnight.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace GameModel
+{
+    public class EnemyKnight : IUnit
+    {
+        public double MaxLife { get; set; }
+        public double ActualLife { get; set; }
+
+        public int Damage { get { return Level * 10; } }
+
+        public int Speed { get; set; }
+        public int Tick { get; set; }
+        public Point Position { get; set; }
+        public int Level { get; set; }
+
+        public Geometry Area
+        {
+            get
+            {
+                return new RectangleGeometry(new Rect(Position.X, Position.Y, Config.TileSize - 15, Config.TileSize));
+            }
+        }
+
+        private int speedX;
+
+        public EnemyKnight(double X, double Y){
+            this.speedX = -5;
+
+            this.MaxLife = 100;
+            this.ActualLife = MaxLife;
+            this.Speed = 2;
+            this.Tick = 0;
+            this.Position = new Point(X, Y);
+            this.Level = 1;
+
+        }
+
+
+
+        public void Collision()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsCollision(IGameItem other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
