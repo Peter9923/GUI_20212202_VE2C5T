dc01a5741373bdfb30570ee43cee9b141f9ab5f8
PeterSzilagyi <szilagyi.peter98@gmail.com>
2022-04-24 06:58:55 +0200

    Build current version.

--- Knights_And_Fields/Game/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ Knights_And_Fields/Game/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace Game {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Game;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Game;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- Knights_And_Fields/Menu/obj/Debug/net5.0-windows/Windows/ScoreboardWindowFolder/Scoreboard.g.i.cs
+++ Knights_And_Fields/Menu/obj/Debug/net5.0-windows/Windows/ScoreboardWindowFolder/Scoreboard.g.i.cs
@@ -47,13 +47,13 @@ namespace Menu.Windows {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Menu;V1.0.0.0;component/windows/scoreboardwindowfolder/scoreboard.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Menu;component/windows/scoreboardwindowfolder/scoreboard.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Windows\ScoreboardWindowFolder\Scoreboard.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -63,7 +63,7 @@ namespace Menu.Windows {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
