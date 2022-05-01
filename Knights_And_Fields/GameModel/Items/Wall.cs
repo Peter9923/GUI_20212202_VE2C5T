5f7c18ec762fbd6b9d265719e2319720cae2512e
Ahorix <ahorixgame@gmail.com>
2022-05-01 13:49:37 +0200

    created wall model

---
+++ Knights_And_Fields/GameModel/Items/Wall.cs
@@ -0,0 +1,39 @@
+﻿using GameModel.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace GameModel.Items
+{
+    class Wall : IAllied
+    {
+        public int UpgradeCost => throw new NotImplementedException();
+
+        public int Cost => throw new NotImplementedException();
+
+        public double MaxLife { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double ActualLife { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Level { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int AttackAnimationIndex { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int WalkingIndex { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool ShouldAttack { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool ShouldWalk { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public double Damage => throw new NotImplementedException();
+
+        public Point Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Geometry RealArea => throw new NotImplementedException();
+
+        public Geometry CollisionArea => throw new NotImplementedException();
+
+        public bool IsCollision(IGameItem other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
